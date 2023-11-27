09cfdef8c5aaae78fac6424fcb934918bd0ad0c4
Artyom <106510046+TYom4ikk@users.noreply.github.com>
2023-11-27 09:29:24 +0500

    Add files via upload
    NEEDED!

---
+++ Nike-Sneakers-main/assets/js/script.js
@@ -0,0 +1,33 @@
+let circle = document.getElementsByClassName[0];
+let circle_clicked = document.getElementsByClassName[0];
+
+circle.addEventListener("click", function() {
+    console.log("Нажате на цвет чёрный!");
+    
+});
+
+let sneakers = [{
+    "image" : "../img/sneakers/sneakers-1.png",
+    "text" : "Nike Air",
+    "price": "$180,00",
+    "ex_price": "$280,00"
+},
+{
+    "image" : "../img/sneakers/sneakers-2.png",
+    "text" : "Air Jordan",
+    "price": "$199,00",
+    "ex_price": "$280,00"
+},
+{
+    "image" :"../img/sneakers/sneakers-3.png",
+    "text" : "Nike Netro",
+    "price": "$135,00",
+    "ex_price": "$280,00"
+},
+{
+    "image":"../img/sneakers/sneakers-4.png",
+    "text" : "Air Spain",
+    "price": "$149,00",
+    "ex_price": "$280,00"
+}
+];
