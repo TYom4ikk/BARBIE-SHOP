46074bc17ccae34041598d052b6b4197bc30081f
TYom4ikk <Zhakubekov20032006@gmail.com>
2023-12-20 01:05:16 +0500

    Trying to do final layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,6 +39,61 @@ let li = document; //Искать все документы
 
 alert("Welcome to Barbie site");
 
+let dolls = [
+    {
+        "image":"assets/image/dolls/doll_1.png",
+        "text" : "doll_1"
+    },
+    {
+        "image":"assets/image/dolls/doll_2.png",
+        "text" : "doll_2"
+    },
+    {
+        "image":"assets/image/dolls/doll_3.png",
+        "text" : "doll_3"
+    },
+    {
+        "image":"assets/image/dolls/doll_4.png",
+        "text" : "doll_4"
+    },
+    {
+        "image":"assets/image/dolls/doll_5.png",
+        "text" : "doll_5"
+    },
+    {
+        "image":"assets/image/dolls/doll_6.png",
+        "text" : "doll_6"
+    }
+]
+
+
+let currentIndex = 0;
+
+function showImages() {
+    let imageContainer = document.getElementById("imageContainer");
+    imageContainer.innerHTML = ""; // Очищаем контейнер перед выводом новых картинок
+
+    for (let i = currentIndex; i < currentIndex + 4; i++) {
+        if (i < dolls.length) {
+            let doll = dolls[i];
+            let img = document.createElement("img");
+            img.src = doll.image;
+            img.alt = doll.text;
+            imageContainer.appendChild(img);
+        }
+    }
+}
+
+function shiftImages() {
+    if (currentIndex + 1 < dolls.length) {
+        currentIndex++;
+        showImages();
+    }
+}
+
+// Вызываем функцию для первичного вывода картинок
+showImages();
+
 let navigation = [{
     "image" : "assets/image/top panel/fi-rr-home.png",
     "text" : "Home"
